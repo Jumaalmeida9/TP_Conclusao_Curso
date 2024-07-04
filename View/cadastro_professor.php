7483d76cd80be5af491fc93c09db87c9e2be9c5e
jumaalmeida9 <juma_almeida@outlook.com>
2024-07-03 22:03:51 -0300

    aula 03/07/2024

---
+++ View/cadastro_professor.php
@@ -0,0 +1,28 @@
+<html>
+    <head> 
+        <title>Cadastro Professores</title>
+    </head>
+    <body>
+        <h2>Cadastro Professores</h2>
+
+        
+        <form action="" method="post">
+            
+        <label for="departamento">Departamento:</label>
+        <input type="text" id="departamento" nome="departamento">
+
+        <label for="salario_base">Salario Base:</label>
+        <input type="number" step="0.01" id="salario_base">
+
+        <label for="beneficios">Benefícios:</label>
+        <input type="number" step="0.01" id="beneficios">
+        
+        <label for="Descontos">Descontos:</label>
+        <input type="number" step="0.01" id="descontos">
+
+        <button type= "submit">Adicionar Professor</button>
+
+
+        </form>
+    </body>
+</html>
---
+++ View/usuarios.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+
+    </head>
+   
+   
+
+    <title>Cadastro de Usuário</title>
+
+
+<body>
+    
+    <form action="" method="post">
+
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome" required><br><br>
+        
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br><br>
+        
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha" required><br><br>
+
+        <select id="papel" name="papel">
+
+            <option value="coordenador">Coordenador</option>
+
+        </select>
+        
+        <button type="submit">Adicionar Usuário</button>
+
+    </form>
+</body>
+</html>
+
+   
